9462e472619d572e3dea1029379b47ea8931a0bb
Madan Singh <madansingh7@yahoo.com>
2026-01-01 02:36:03 +0530

    Add infix to postfix conversion program in C
    Implement a C program to convert infix expressions to postfix using a stack.

---
+++ LAB_EXAM/infix_postfix.c
@@ -0,0 +1,78 @@
+//Write a C program to covert infix to postix using stack as the data structure
+#include <stdio.h>
+#include<ctype.h>
+#define N 20
+typedef struct {
+    int stk[N];
+    int top;
+}S;
+
+void push(S *s, int ele){
+    if(s->top == N-1){
+        printf("Overflow\n");
+        return;
+    }
+    s->stk[++(s->top)]=ele;
+}
+
+char pop(S *s){
+     if(s->top == -1){
+        printf("Underflow\n");
+        return -1;
+        }
+    return s->stk[(s->top)--];
+}
+
+int precedence(char op)
+{
+    switch(op)
+    {
+        case '(': return 1;
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': 
+        case '%': return 3;
+        case '^':
+        case '$': return 4;
+    }
+}
+
+int main() {
+    S s1;
+    s1.top=-1;
+    char infix[N], postfix[N], ch, c;
+    int i = 0, j = 0;
+    printf("Enter a valid infix expression: ");
+    scanf("%s", infix);
+    while(infix[i] != '\0'){
+        ch=infix[i++];
+        if(ch=='('){
+            push(&s1,ch);
+        }
+        else if(isalnum(ch)){
+            postfix[j++]=ch;
+        }
+        else if (ch==')'){
+            while((c=pop(&s1))!='('){
+                postfix[j++]=c;
+            }
+        }
+        else{
+        while(s1.top != -1 && 
+     ((precedence(ch) < precedence(s1.stk[s1.top])) ||
+     (precedence(ch) == precedence(s1.stk[s1.top]) && ch != '^' && ch != '$'))){
+                postfix[j++]=pop(&s1);
+            }
+            push(&s1,ch);
+        }
+    }
+    while(s1.top != -1)
+    {
+        postfix[j++] = pop(&s1);
+    }
+    postfix[j] = '\0';
+    printf("Postfix of infix expression is %s = %s\n", infix, postfix);
+
+    return 0;
+}
